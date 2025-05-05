0251e1f757d6bd46f8b601443b40e975e6ad5a1d
빠빠재 <35914920+nnxi@users.noreply.github.com>
2025-05-06 03:05:28 +0900

    Update and rename merge_sort.cpp to codes/merge_sort.cpp

--- codes/merge_sort.cpp
+++ codes/merge_sort.cpp
@@ -25,8 +25,8 @@ int main() {
 
 	double time = (double)(end - start) / CLOCKS_PER_SEC;
 
-	cout << "°ËÁõ¿ë Á¤·Ä ÈÄ a[99999] : " << a[99999] << "\n";
-	cout << "½ÇÇà½Ã°£ : " << time << "ÃÊ";
+	cout << "ê²€ì¦ìš© ì •ë ¬ í›„ a[99999] : " << a[99999] << "\n";
+	cout << "ì‹¤í–‰ì‹œê°„ : " << time << "ì´ˆ";
 }
 
 void merge_sort(int arr[], int left, int right) {
